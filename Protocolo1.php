87c95a509b486a95600e2ee271e1ce5a57e0c111
Filipe Gomes <filippedocs@gmail.com>
2018-03-05 16:49:54 -0300

    Inicial
    Upando arquivos

---
+++ Protocolo1.php
@@ -0,0 +1,40 @@
+﻿	<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Protocolo Proace</title>
+<link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+
+<body>
+<pre style="text-align: right"> 
+
+<?php 
+	
+	echo "<p>Hoje é " . date("l") . "</p>";
+
+	?>
+     </pre>   
+<span style="font-size: 30px; text-align: center;">Programa de Protocolos Proace </span>
+
+
+<form method="post" action="Protocolo2.php">
+<p>Nome:      
+  <input type="text" size="35" maxlength="256" name="username">
+  </p>
+  <p>Operador: 
+    <input type="text" size="33" maxlength="10" name="operador">
+  </p>
+  <p><span style="font-size: 28">Documento</span>:
+<textarea name="document" rows="6" cols="30"></textarea> 
+  </p>
+  <p>
+    <input type="submit">    
+  </p>
+</form>
+
+</body>
+
+
+
+</html>
---
+++ protocoloin.php
@@ -0,0 +1,52 @@
+﻿	<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Protocolo Proace</title>
+<link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+
+<body>
+<pre style="text-align: right"> 
+
+<?php 
+	
+	echo "<p>Hoje é " . date("l") . "</p>";
+
+	?>
+     </pre>   
+<span style="font-size: 30px; text-align: center;">Programa de Protocolos Proace </span>
+
+
+<form method="post" action="Protocolo2.php">
+<p>Nome:      
+  <input type="text" size="35" maxlength="256" name="username">
+  </p>
+  <p>  Operador:
+  <input type="text" size="33" maxlength="256" name="operador">
+  </p>
+  <p><span style="font-size: 20px; font-style: normal;">Tipo de Documento: </span>   </p>
+  <p><!--<input type="radio" name="tipo_documento" value="1">
+    1º Cadastro PAE-->
+      <input type="radio" name="tipo_documento" value="2">  
+  Dados Bancários
+  
+  <input type="radio" name="tipo_documento" value="3">
+ Declaração de Frequência
+
+  <input type="radio" name="tipo_documento" value="4">
+  Termo de Concessão e Escolha de Benefício
+
+  <input type="radio" name="tipo_documento" value="5">
+  Vinculação em Projeto
+
+  <p>    
+    <input type="submit">    
+</p>
+</form>
+<form method="post" action="Protocolo1.php"><input type="submit" name="tipo_documento2" value=" Clique aqui para outro tipo de documentação"></form>
+</body>
+
+
+
+</html>
