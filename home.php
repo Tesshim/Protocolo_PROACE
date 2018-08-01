a2e7bd271238df66b98ada778d0fe01ee2109bc8
Filipe Gomes <filippedocs@gmail.com>
2018-08-01 15:55:12 -0300

    Upando novo modelo

---
+++ home.php
@@ -0,0 +1,32 @@
+<div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">PROTOCOLO</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Nome Aluno</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nome">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Operador</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Operador">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Documento</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Documento">
+                </div>
+                <div class="form-group">
+                  <label for="comment">Comment:</label>
+                  <textarea class="form-control" rows="5" id="comment"></textarea>
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Salvar</button>
+              </div>
+            </form>
+          </div>
