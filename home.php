53fa5c141bbbacdf68034cefbf6a805a9d28d572
Filipe Gomes <filippedocs@gmail.com>
2018-08-07 14:48:53 -0300

    cadastro de protocolos na home

--- home.php
+++ home.php
@@ -1,32 +1,91 @@
+<?php
+    include('dbconnection.php');
+
+    $sql_select = "SELECT * FROM tb_aluno ORDER BY nome_aluno";
+    $sql_aluno_select = mysqli_query($conn, $sql_select);
+
+?>
+
 <div class="box box-primary">
             <div class="box-header with-border">
               <h3 class="box-title">PROTOCOLO</h3>
             </div>
             <!-- /.box-header -->
             <!-- form start -->
-            <form role="form">
+            <form method="POST" action="">
               <div class="box-body">
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Nome Aluno</label>
-                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nome">
-                </div>
+                <label>Aluno</label>
+                <select name="aluno" id="aluno" class="form-control select2 <?php echo $class;?>" style="width: 100%;">
+                  <?php 
+
+                while($aluno_select = mysqli_fetch_array($sql_aluno_select, MYSQLI_BOTH))
+                { ?>
+
+                    <option value="<?php echo $aluno_select['id_aluno']; ?>"><?php echo $aluno_select['nome_aluno'];?></option>
+
+               <?php } ?>
+                </select>
+              </div>
+
                 <div class="form-group">
                   <label for="exampleInputPassword1">Operador</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Operador">
+                  <input name="operador" id="operador" type="text" class="form-control" placeholder="Operador">
                 </div>
+
                 <div class="form-group">
-                  <label for="exampleInputPassword1">Documento</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Documento">
+                  <label>Documento</label>
+                  <select name="documento" id="documento" class="form-control">
+                    <option>Dados Bancários</option>
+                    <option>Declaração de Frequência</option>
+                    <option>Termo de Concessão e Escolha de Benefício</option>
+                    <option>Vinculação em Projeto</option>
+                    <option>Outro</option>
+                  </select>
                 </div>
+
                 <div class="form-group">
-                  <label for="comment">Comment:</label>
-                  <textarea class="form-control" rows="5" id="comment"></textarea>
+                  <label for="comment">Comentário:</label>
+                  <textarea name="coment" id="coment" class="form-control" rows="5"></textarea>
                 </div>
               </div>
               <!-- /.box-body -->
 
               <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Salvar</button>
+                <button name="enviar" id="enviar" type="submit" class="btn btn-primary">Salvar</button>
               </div>
             </form>
           </div>
+
+          <?php  
+              if (isset($_POST['enviar'])) {
+                $nome = $_POST['aluno'];
+                $operador = $_POST['operador'];
+                $documento = $_POST['documento'];
+                $coment = $_POST['coment'];
+
+                $sql = "INSERT INTO tb_documentos
+                (id_aluno, operador, documento, comentario) values ('$nome', '$operador', '$documento', '$coment')";
+
+                
+
+                if(mysqli_query($conn, $sql))  
+                {
+                  $sql_select = "SELECT * FROM tb_documentos ORDER BY id_protocolo DESC LIMIT 1";
+                  $sql_protocolo_select = mysqli_query($conn, $sql_select);
+                  $protocolo_select = mysqli_fetch_array($sql_protocolo_select, MYSQLI_BOTH);
+
+                  echo '<div class="alert alert-success" role="alert">
+                  Documento '.$protocolo_select['id_protocolo'].' cadastrado com sucesso! </div>';
+
+                }else
+                {
+                  echo '<div class="alert alert-danger" role="alert">
+                  Documento não cadastrado!
+                  </div>';
+                }
+              }
+
+
+
+              ?>
--- index.php
+++ index.php
@@ -27,6 +27,10 @@
   <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
   <!-- bootstrap wysihtml5 - text editor -->
   <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <!-- daterange picker -->
+  <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="bower_components/select2/dist/css/select2.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -150,40 +154,6 @@
 
     <!-- MAIN CONTENT -->
     <section class="content">
-      <!-- Small boxes (Stat box) -->
-      <div class="row">
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-aqua">
-            <div class="inner">
-              <h3>150</h3>
-
-              <p>Projetos Cadastrados</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>Alunos Cadastrados</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-      </div>
-      <!-- /.row -->
 
       <?php 
       ini_set( 'display_errors', 0 );
@@ -234,11 +204,16 @@
 <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- jQuery Knob Chart -->
 <script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<!-- jQuery 3 -->
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
 <!-- daterangepicker -->
 <script src="bower_components/moment/min/moment.min.js"></script>
 <script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
 <!-- datepicker -->
 <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<!-- DataTables -->
+<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 <!-- Bootstrap WYSIHTML5 -->
 <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- Slimscroll -->
@@ -251,5 +226,94 @@
 <script src="dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
+<!-- Select2 -->
+<script src="bower_components/select2/dist/js/select2.full.min.js"></script>
+<!-- InputMask -->
+<script src="plugins/input-mask/jquery.inputmask.js"></script>
+<script src="plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
+<script src="plugins/input-mask/jquery.inputmask.extensions.js"></script>
+
+<script>
+  $(function () {
+    $('#example1').DataTable()
+    $('#example2').DataTable({
+      'paging'      : true,
+      'lengthChange': false,
+      'searching'   : false,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : false
+    })
+  })
+</script>
+
+<script>
+  $(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+
+    //Datemask dd/mm/yyyy
+    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+    //Datemask2 mm/dd/yyyy
+    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+    //Money Euro
+    $('[data-mask]').inputmask()
+
+    //Date range picker
+    $('#reservation').daterangepicker()
+    //Date range picker with time picker
+    $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
+    //Date range as a button
+    $('#daterange-btn').daterangepicker(
+      {
+        ranges   : {
+          'Today'       : [moment(), moment()],
+          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        startDate: moment().subtract(29, 'days'),
+        endDate  : moment()
+      },
+      function (start, end) {
+        $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+      }
+    )
+
+    //Date picker
+    $('#datepicker').datepicker({
+      autoclose: true
+    })
+
+    //iCheck for checkbox and radio inputs
+    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+      checkboxClass: 'icheckbox_minimal-blue',
+      radioClass   : 'iradio_minimal-blue'
+    })
+    //Red color scheme for iCheck
+    $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+      checkboxClass: 'icheckbox_minimal-red',
+      radioClass   : 'iradio_minimal-red'
+    })
+    //Flat red color scheme for iCheck
+    $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+      checkboxClass: 'icheckbox_flat-green',
+      radioClass   : 'iradio_flat-green'
+    })
+
+    //Colorpicker
+    $('.my-colorpicker1').colorpicker()
+    //color picker with addon
+    $('.my-colorpicker2').colorpicker()
+
+    //Timepicker
+    $('.timepicker').timepicker({
+      showInputs: false
+    })
+  })
+</script>
+
 </body>
 </html>
